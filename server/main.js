1130877d6e9d7f2cd21a58699d4c59f2cd7e7e6d
zhangming <shizuka@mocait.com>
2018-07-16 16:07:04 +0800

    server for test after build

---
+++ server/main.js
@@ -0,0 +1,32 @@
+//
+//  Copyright (C) 2016-2018  dvTrend, Inc - All Rights Reserved
+//
+//  author: Wendong
+//
+
+var app = require('express')();
+var httpServer = require('http').Server(app);
+var path = require('path');
+var ip = require('ip');
+
+const httpport    = process.env.PORT || 12321;
+
+app.use(require('express').static(path.join('../build')));
+
+// Just for testing, will remove in product
+app.get('/test', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from server!"}');
+});
+
+
+app.get('*', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Do you want a index.html?"}');
+});
+
+httpServer.listen(httpport, function() {
+    console.log('listening on ' + ip.address() + ":" + httpport);
+});
+
+
