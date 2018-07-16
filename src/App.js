0a906d18e40b51ac9fa383423ba0418cfa5768a7
zhangming <shizuka@mocait.com>
2018-07-16 09:24:10 +0800

    less and css test but not use css modules

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'antd-mobile';
 import logo from './logo.svg';
+
 import './App.css';
+import './Appless.less'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Icon type="success" size="lg"/>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">This is a button</Button>
       </div>
     );
   }
