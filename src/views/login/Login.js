5a70dd5e9b8f8aa92e5f839c75ca2f519c24668e
zhangming <shizuka@mocait.com>
2018-07-17 12:56:53 +0800

    add jquery ajax

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import React, { Component } from 'react'
+import { Button } from 'antd-mobile'
+import $ from 'jquery'
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="Login">
-        <Button type="primary">Login</Button>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    onLoginClick = () => {
+        const _this = this;
+        $.ajax({
+            type: "get",
+            url: "http://apis.juhe.cn/cook/query?key=dd64a7d8a30f6bccb0589d233f2f7861&rn=5&pn=0&menu=2",
+            dataType: "jsonp",
+            success: function (data) {
+                console.log(data);
+                _this.setState({
+                    items: data.result.data
+                });
+            },
+            error: function () {
+
+            },
+            beforeSend: function () {
+
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <Button type="primary" onClick={this.onLoginClick.bind(this)}>Login</Button>
+                <ul>
+                    {
+                        this.state.items.map(item => (
+                            (<li key={item.id}>{item.title}</li>)
+                        ))
+                    }
+                </ul>
+            </div>
+        );
+    }
 }
 export default Login;
