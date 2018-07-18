f42623b0d971dc56164b97fef307b58ae36d00bc
zhangming <shizuka@mocait.com>
2018-07-18 10:51:29 +0800

    add react-weui for test

--- src/views/stats/Stats.js
+++ src/views/stats/Stats.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import { Button, FormCell, CellHeader, CellBody, Label, Input, CellFooter, Form } from 'react-weui';
+//import styles
+import 'weui';
+import 'react-weui/build/packages/react-weui.css';
+//import { Button} from 'antd-mobile'
 
 class Stats extends Component {
-  render() {
-    return (
-      <div className="Stats">
-        <Button type="primary">Stats</Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Stats">
+                <Button>hello wechat</Button>
+                <Form>
+                    {/* <div style={{background:'#eee'}}> */}
+                    <FormCell vcode>
+                        <CellHeader>
+                            <Label>Phone</Label>
+                        </CellHeader>
+                        <CellBody>
+                            <Input type="tel" placeholder="请输入手机号" />
+                        </CellBody>
+                        <CellFooter>
+                            <Button type="vcode">获取验证码</Button>
+                        </CellFooter>
+                    </FormCell>
+                    {/* </div> */}
+                </Form>
+            </div>
+        );
+    }
 }
 export default Stats;
+
